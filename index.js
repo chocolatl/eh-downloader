90ec0ad4ba5ae2dff2d28c1a9134936bc4ae1d89
chocolatl <choclatl@outlook.com>
2017-12-10 16:17:42 +0800

    将downloadFile函数独立为模块

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const EventEmitter = require('events');
 
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
-const request = require('request');
 const yaml = require('js-yaml');
 const deepAssign = require('deep-assign');
 const mkdirp = require('mkdirp');
@@ -30,52 +29,9 @@ function requestHTML(url, userOptions) {
 }
 
 function downloadFile(url, path) {
+    const downloadFile = require('./lib/download-file');
 
-    return new Promise(function(resolve, reject) {
-
-        let stream = fs.createWriteStream(path);
-        let donwloadTimeout = 100000;
-        let timerId;
-
-        let options = {
-            timeout: 60000,
-            headers: {
-                'user-agent': ''
-            }
-        }
-
-        // 此处的timeout为等待服务器响应的时间
-        request.get(url, options).on('error', function(err) {
-            return reject(err);
-        }).on('response', function(response) {
-
-            if(response.statusCode !== 200) {
-                return reject(new Error(`Response Error. HTTP Status Code: ${response.statusCode}.`));
-            }
-
-            // 到达等待时间下载未完成，销毁可写流并发出错误事件
-            timerId = setTimeout(function() {
-                stream.destroy(new Error('Download Timeout.'));
-            }, donwloadTimeout);
-
-        }).pipe(stream);
-    
-        stream.on('error', function(err) {
-
-            // 出错时清除时钟
-            clearTimeout(timerId);
-
-            return reject(err);
-        });
-    
-        stream.on('finish', function() {
-
-            // 下载完成清除时钟
-            clearTimeout(timerId);
-
-            return resolve();
-        });
-    });
+    return downloadFile(url, path);
 }
 
 function getGalleryTitle(detailsPageURL) {
